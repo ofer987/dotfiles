73ba1723365d891431538b47ee31c474db302852
Dan Jakob Ofer <ofer987@gmail.com>
2015-04-02 09:08:30 -0400

    Install files from my forked repository: ofer987/dotfiles

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    git clone --depth=1 https://github.com/ofer987/dotfiles.git "$HOME/.yadr"
+
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
