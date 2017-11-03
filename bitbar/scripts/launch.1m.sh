e1a4ca96ec337be9abbd1f39956985cbca05f440
Dan Jakob Ofer <dan@ofer.to>
2017-11-02 22:28:34 -0400

    Removed unused bitbar scripts

--- bitbar/scripts/launch.1m.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-
-/usr/local/bin/fish -c '~/bitbar/internal/melbourne_time' 2> /dev/null
--- bitbar/scripts/oz.1d.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-
-/usr/local/bin/fish -c '~/bitbar/internal/to_oz' 2> /dev/null
