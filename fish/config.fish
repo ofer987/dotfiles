a957d7856c92de5920894e2d0104744c36f8aba5
Dan Jakob Ofer <dan@ofer.to>
2022-02-10 15:14:18 -0500

    feat(ccd): change into a file's directory

--- fish/config.fish
+++ fish/config.fish
@@ -146,3 +146,44 @@ end
 function root
   cd (project)
 end
+
+# Get the file/directory's parent
+function get_parent
+  set items (string split / $argv[1])
+  set count (count $items)
+
+  set count (math $count - 1)
+
+  set i 0
+  set result ""
+  for item in $items
+    if test -n $item && test $i -lt $count
+      set result "$result/$item"
+    end
+
+    set i (math $i + 1)
+  end
+
+  echo $result
+end
+
+# cd into the file's directory
+function ccd
+  set dir $argv[1];
+
+  if test -z $dir
+    return 1;
+  end
+
+  while test -n $dir
+    if test -d $dir
+      cd $dir
+      return 0
+    else if test -e $dir
+      set parent (get_parent $dir)
+      set dir $parent
+    end
+  end
+
+  return 1
+end
