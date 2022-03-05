9b3d614233c5ece4be86f28ba6cb56cbabd8ace5
Dan Jakob Ofer <dan@ofer.to>
2022-03-05 06:39:47 -0500

    Create gem

---
+++ scripts/git-scripts/lib/git_scripts.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module GitScripts
+  require_relative 'git_scripts/version'
+  require_relative 'git_scripts/git'
+
+  class Error < StandardError; end
+  # Your code goes here...
+end
