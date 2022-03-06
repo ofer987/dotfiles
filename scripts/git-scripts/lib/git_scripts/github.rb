d20f02591bd6d8c0582aabe811cb0bc3fea63cc4
Dan Jakob Ofer <dan@ofer.to>
2022-03-06 06:40:16 -0500

    Bug fixes

--- scripts/git-scripts/lib/git_scripts/github.rb
+++ scripts/git-scripts/lib/git_scripts/github.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-# require 'pry-byebug'
-
 class GitHub
   REGEX = %r{github\.com[/:](.*)\.git\b}
-  # REGEX_HTTPS = /github\.com\/(.*)\.git\b/
 
   def self.github_repos
     repos = `git remote -v`.chomp.split("\n")
 
-    # binding.pry
     repos
       .map { |item| REGEX.match(item) }
       .filter { |item| item.size >= 2 }
       .map { |item| item[1] }
       .uniq
+  end
+
+  def self.create_pull_request_url(branch_name)
+    "https://github.com/#{GitHub.github_repos.first}/compare/#{Git.merge_branch_name}...#{branch_name}"
   end
 
   def initialize(username, password)
@@ -36,10 +36,5 @@ class GitHub
       .select { |item| item.user.login == @username && item.head.ref == branch_name }
       .sort_by(&:updated_at)
       .reverse
-  end
-
-  def self.create_branch_url(branch_name)
-    # https://github.com/tr/digital_emcm-web/compare/develop...DPT-1398
-    "#{Git.repo_url}/compare/#{Git.merge_branch_name}...#{branch_name}"
   end
 end
