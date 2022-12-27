4ccc7c276d20701fedf5e87ad8344edad3ce2506
Dan Jakob Ofer <dan@ofer.to>
2022-12-27 12:25:02 -0500

    chore(LaunchBar): Localhost Action can open HTTPS

--- LaunchBar/open.lbaction/Contents/Scripts/default.js
+++ LaunchBar/open.lbaction/Contents/Scripts/default.js
@@ -1,11 +1,26 @@
 // LaunchBar Action Script
 
+let isHttps = (argument) => {
+  if (argument.toLowerCase() === 'https') {
+    return true;
+  }
+
+  return false;
+}
+
 function run(argument) {
   if (!argument) {
     return;
   }
 
   args = argument.split(" ");
+
+  let schema = 'http';
+  if (isHttps(args[0])) {
+    schema = 'https';
+    args = args.slice(1);
+  }
+
   if (args.length === 2) {
     path = args[1];
     if (path.startsWith("/")) {
@@ -13,9 +28,9 @@ function run(argument) {
     }
     path = encodeURI(path);
 
-    uri = `http://localhost:${args[0]}/${path}`;
+    uri = `${schema}://localhost:${args[0]}/${path}`;
   } else {
-    uri = `http://localhost:${args[0]}`;
+    uri = `${schema}://localhost:${args[0]}`;
   }
 
   LaunchBar.openURL(uri);
