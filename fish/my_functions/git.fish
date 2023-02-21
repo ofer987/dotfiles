64f4a586dc979d032c746527f8ab2042206ddee7
Dan Jakob Ofer <dan@ofer.to>
2023-02-21 17:37:47 -0500

    chore(fish): git tab completion

--- fish/my_functions/git.fish
+++ fish/my_functions/git.fish
@@ -2,8 +2,16 @@
 function project
   git rev-parse --show-toplevel 2> /dev/null
 end
-
 # Change directory into Project root
 function root
   cd (project)
+end
+
+# Configure custom git commands to be completed using Fish's tab completion
+for file in (find ~/.yadr/**/git-* -type f)
+  if test -x $file
+    set command_name (string replace -a -r '^.*git-' '' $file)
+
+    complete --no-files git -a "__fish_git_complete_custom_command $command_name"
+  end
 end
